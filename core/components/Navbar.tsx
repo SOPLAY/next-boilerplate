8fe3d27ff97774dadf798955be2a9640b0a7c2f9
soplay <tkdgns1341@gmail.com>
2022-06-09 01:54:26 +0900

    헤더 + 가입 페이지 + 리디렉션 구현

--- core/components/Navbar.tsx
+++ core/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
-import React from 'react';
-
+import React, { useState } from 'react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 const Navbar = () => {
   const { data, status } = useSession();
   let text = '';
@@ -19,7 +19,7 @@ const Navbar = () => {
           { title: 'Sign up', href: '#' },
           {
             title: 'Sign in',
-            href: '#',
+            href: '/api/auth/signin',
           },
         ];
 
@@ -32,27 +32,57 @@ const Navbar = () => {
     { title: 'Teg1', href: '#' },
     { title: 'Teg2', href: '#' },
   ];
+  const [menuToggle, setMenuToggle] = useState(false);
   return (
     <>
-      <nav className="backdrop-blur-3xl flex w-full h-12 px-5 items-center justify-between text-base sticky top-0 ">
-        <ul className="flex ">
-          {navMenu.map((v, i) => (
-            <li key={`NavBar_${i}`} className="mr-5 ">
-              <Link href={v.href}>{v.title}</Link>
-            </li>
-          ))}
-        </ul>
-        <div>
-          <ul className="flex">
-            {loginMenu.map((value, index) => (
-              <li
-                key={`loginMenu${index}`}
-                className="ml-5 px-3 py-1 rounded-md hover:bg-yellow-300 hover:text-gray-800 "
-              >
-                <Link href={value.href}>{value.title}</Link>
-              </li>
-            ))}
-          </ul>
+      <nav className="bg-blue-200 bg-opacity-40 fixed top-0 w-full">
+        <div className="">
+          <div className="mx-5 backdrop-blur-3xl flex h-12 items-center justify-between text-lg ">
+            <ul className="flex ">
+              {navMenu.map((v, i) => (
+                <li
+                  key={`NavBar_${i}`}
+                  className={`mr-5  ${i > 0 && 'hidden md:flex '}`}
+                >
+                  <Link href={v.href}>{v.title}</Link>
+                </li>
+              ))}
+            </ul>
+            <div>
+              <ul className="hidden md:flex">
+                {loginMenu.map((value, index) => (
+                  <li
+                    key={`loginMenu${index}`}
+                    className="ml-5 px-3 py-1 rounded-md hover:bg-yellow-300 hover:text-gray-800 "
+                  >
+                    <Link href={value.href}>{value.title}</Link>
+                  </li>
+                ))}
+              </ul>
+              {/* 모바일 일시 */}
+              <div className="md:hidden flex items-center">
+                <button onClick={() => setMenuToggle(!menuToggle)}>
+                  {!menuToggle ? (
+                    <MenuIcon className="w-6 h-6" />
+                  ) : (
+                    <XIcon className="w-6 h-6" />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+          {menuToggle && (
+            <ul className="md:hidden ">
+              {[
+                ...navMenu.filter((value, index) => index > 0),
+                ...loginMenu.reverse(),
+              ].map((value, index) => (
+                <li className="ml-2 py-1 text-base" key={`mobileMenu_${index}`}>
+                  <Link href={value.href}>{value.title}</Link>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </nav>
     </>
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,22 +5,30 @@ export default nextAuth({
   providers: [
     Credentials({
       // The name to display on the sign in form (e.g. "Sign in with...")
-      name: 'Credentials',
+      name: '이메일, 패스워드 방식',
       // The credentials is used to generate a suitable form on the sign in page.
       // You can specify whatever fields you are expecting to be submitted.
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: '유저네임', type: 'text', placeholder: 'jsmith' },
+        useremail: {
+          label: '이메일',
+          type: 'email',
+          placeholder: 'test@gmail.com',
+        },
         password: { label: '패스워드', type: 'password' },
       },
 
       //인증 부분
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
-        const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' };
+        // const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' };
 
-        if (user) {
+        if (
+          credentials?.useremail === 'test@gmail.com' &&
+          credentials.password === '1q2w3e4r!'
+        ) {
+          const user = { id: 1, name: 'test', email: credentials.useremail };
           // Any object returned will be saved in `user` property of the JWT
 
           return user;
